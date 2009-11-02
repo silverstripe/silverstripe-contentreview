9bc51d9ce97c266ab6949fc72a45344da77c0e31
Tom Rix <tom@rixth.org>
2009-11-02 04:02:57 +0000

    Added content review module

---
+++ README.md
@@ -0,0 +1,20 @@
+# Content Review module
+
+## Maintainer Contact
+* Tom Rix (Nickname: trix)
+  <tom (at) silverstripe (dot) com>
+
+## Requirements
+ * SilverStripe 2.3 or newer
+
+
+## Installation
+
+Drop it into your installation folder, and refresh your database schema
+through `http://<your-host>/dev/build`.
+
+## Usage
+
+When you open a page in the CMS, there will now be a Review tab.
+
+
---
+++ code/SiteTreeContentReview.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Set dates at which content needs to be reviewed and provide
+ * a report and emails to alert to content needing review
+ *
+ * @package contentreview
+ */
+class SiteTreeCMSWorkflow extends DataObjectDecorator implements PermissionProvider {
+	
+	function extraStatics() {
+		return array(
+			'db' => array(
+				"ReviewPeriodDays" => "Int",
+				"NextReviewDate" => "Date",
+				'ReviewNotes' => 'Text'
+			),
+			'has_one' => array(
+				'Owner' => 'Member'
+			),
+		);
+	}
+	
+	function getOwnerName() {
+		if($this->owner->Owner()) return $this->owner->Owner()->FirstName . ' ' . $this->owner->Owner()->Surname;
+	}
+	
+	public function updateCMSFields(&$fields) {
+		if(Permission::check("EDIT_CONTENT_REVIEW_FIELDS")) {
+			
+			$cmsUsers = Permission::get_members_by_permission(array("CMS_ACCESS_CMSMain", "ADMIN"));
+			
+			$fields->addFieldsToTab("Root.Review", array(
+				new HeaderField(_t('SiteTreeCMSWorkflow.REVIEWHEADER', "Content review"), 2),
+				new DropdownField("OwnerID", _t("SiteTreeCMSWorkflow.PAGEOWNER", 
+					"Page owner (will be responsible for reviews)"), $cmsUsers->map('ID', 'Title', '(no owner)')),
+				new CalendarDateField("NextReviewDate", _t("SiteTreeCMSWorkflow.NEXTREVIEWDATE",
+					"Next review date (leave blank for no review)")),
+				new DropdownField("ReviewPeriodDays", _t("SiteTreeCMSWorkflow.REVIEWFREQUENCY", 
+					"Review frequency (the review date will be set to this far in the future whenever the page is published.)"), array(
+					0 => "No automatic review date",
+					1 => "1 day",
+					7 => "1 week",
+					30 => "1 month",
+					60 => "2 months",
+					91 => "3 months",
+					121 => "4 months",
+					152 => "5 months",
+					183 => "6 months",
+					365 => "12 months",
+				)),
+				new TextareaField('ReviewNotes', 'Review Notes')
+			));
+		}
+	}
+	
+	function onBeforeWrite() {
+		if($this->owner->ReviewPeriodDays && !$this->owner->NextReviewDate) {
+			$this->owner->NextReviewDate = date('Y-m-d', strtotime('+' . $this->owner->ReviewPeriodDays . ' days'));
+		}
+	}
+	
+	function providePermissions() {
+		return array(
+			"EDIT_CONTENT_REVIEW_FIELDS" => array(
+				'name' => "Set content owners and review dates",
+				'category' => _t('Permissions.CONTENT_CATEGORY', 'Content permissions'),
+				'sort' => 50
+			)
+		);
+	}
+}
