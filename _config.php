eed8ad2918c259c647f89b27b0203cd943492ced
Luke Hudson <lukeletters@gmail.com>
2010-03-01 21:17:11 +0000

    BUGFIX: Check for class_exists in contentreview

--- _config.php
+++ _config.php
@@ -3,7 +3,7 @@
 Object::add_extension('SiteTree', 'SiteTreeContentReview');
 
 
-if(class_exists('Subsite')) {
+if(class_exists('Subsite') && class_exists('SubsiteReportWrapper')){
 	SS_Report::register('ReportAdmin', 'SubsiteReportWrapper("PagesDueForReviewReport")',20);
 } else {
 	SS_Report::register('ReportAdmin', 'PagesDueForReviewReport',20);
