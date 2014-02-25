ea44b0026ec98266f178f1ff6d6dfc584110060d
Stig Lindqvist <stojg.lindqvist@gmail.com>
2014-02-25 21:21:08 +1300

    Fixed the ContentReviewReportTest for real this time, wrong order in assertEquals

--- tests/ContentReviewReportTest.php
+++ tests/ContentReviewReportTest.php
@@ -18,20 +18,21 @@ class ContentReviewReportTest extends FunctionalTest {
 			'ReviewDateBefore' => '12/12/2010'
 		), 'NextReviewDate ASC', false);
 		
-		$this->assertEquals($results->column('Title'), array(
+		$this->assertEquals(array(
 			'Contact Us',
+			'Contact Us Child',
 			'Staff',
 			'About Us',
 			'Home'
-		));
+		), $results->column('Title'));
 		
 		SS_Datetime::set_mock_now('2010-02-13 00:00:00');
 		$results = $report->sourceRecords(array(
 		), 'NextReviewDate ASC', false);
-		$this->assertEquals($results->column('Title'), array(
+		$this->assertEquals(array(
 			'About Us',
 			'Home'
-		));
+		), $results->column('Title'));
 		
 		SS_Datetime::clear_mock_now();
 	}
