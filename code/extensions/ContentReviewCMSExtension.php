0610b12a4a720c3848f94ce1c3c3ded7d4f91e55
scott1702 <scott@silverstripe.com>
2015-11-12 15:56:19 +1300

    Make contentreview an inline form

--- code/extensions/ContentReviewCMSExtension.php
+++ code/extensions/ContentReviewCMSExtension.php
@@ -11,58 +11,8 @@ class ContentReviewCMSExtension extends LeftAndMainExtension
      * @var array
      */
     private static $allowed_actions = array(
-        "reviewed",
-        "AddReviewForm",
+        "savereview"
     );
-
-    /**
-     * Shows a form with review notes.
-     *
-     * @param array $data
-     * @param Form  $form
-     *
-     * @return SS_HTTPResponse
-     * @throws SS_HTTPResponse_Exception
-     */
-    public function reviewed($data, Form $form)
-    {
-        $record = $this->findRecord($data);
-        if (!$record->canEdit()) {
-            return Security::permissionFailure($this->owner);
-        }
-
-        // Populate and respond
-        $form = $this->AddReviewForm();
-        $form->loadDataFrom($record);
-        return $form->forTemplate();
-    }
-
-    /**
-     * Form handler for this page
-     *
-     * @return CMSForm
-     */
-    public function AddReviewForm()
-    {
-        $reviewNotes = TextareaField::create("ReviewNotes", _t("ContentReview.REVIEWNOTES", "Review notes"));
-        $reviewNotes->setDescription(_t("ContentReview.REVIEWNOTESDESCRIPTION ", "Add comments for the content of this page."));
-        $fields = new FieldList();
-        $fields->push(HiddenField::create("ID"));
-        $fields->push($reviewNotes);
-
-        $actions = new FieldList(
-            FormAction::create("save_review", _t("ContentReview.SAVE", "Save"))
-        );
-
-        $form = CMSForm::create($this->owner, "AddReviewForm", $fields, $actions)->setHTMLID("Form_EditForm");
-        $form->setResponseNegotiator($this->owner->getResponseNegotiator());
-        $form->disableDefaultAction();
-
-        // TODO Can't merge $FormAttributes in template at the moment
-        $form->setTemplate($this->owner->getTemplatesWithSuffix("LeftAndMain_EditForm"));
-
-        return $form;
-    }
 
     /**
      * Save the review notes and redirect back to the page edit form.
@@ -74,19 +24,21 @@ class ContentReviewCMSExtension extends LeftAndMainExtension
      *
      * @throws SS_HTTPResponse_Exception
      */
-    public function save_review($data, Form $form)
+    public function savereview($data, Form $form)
     {
         $page = $this->findRecord($data);
         if (!$page->canEdit()) {
             return Security::permissionFailure($this->owner);
         }
 
-        $page->addReviewNote(Member::currentUser(), $data["ReviewNotes"]);
+        $notes = (!empty($data["ReviewNotes"]) ? $data["ReviewNotes"] : _t("ContentReview.NOCOMMENTS", "(no comments)"));
+        $page->addReviewNote(Member::currentUser(), $notes);
         $page->advanceReviewDate();
-
-        return $this->owner->redirect($this->owner->Link("show/" . $page->ID));
+        
+        $this->owner->getResponse()->addHeader("X-Status", _t("ContentReview.REVIEWSUCCESSFUL", "Content reviewed successfully"));
+        return $this->owner->redirectBack();
     }
-
+    
     /**
      * Find the page this form is updating
      *
