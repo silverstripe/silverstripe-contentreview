f64561e1b40fba7c9517280096d42ddfd9eb0e60
Raissa North <raissa@silverstripe.com>
2018-01-09 09:30:20 +1300

    API Remove Translatable compatibility logic

--- src/Compatibility/ContentReviewCompatability.php
+++ src/Compatibility/ContentReviewCompatability.php
@@ -4,13 +4,10 @@ namespace SilverStripe\ContentReview\Compatibility;
 
 use SilverStripe\Core\ClassInfo;
 use SilverStripe\Subsites\Model\Subsite;
-use Translatable;
-
-// @todo add translatable namespace
 
 /**
  * This is a helper class which lets us do things with content review data without subsites
- * and translatable messing our SQL queries up.
+ * messing our SQL queries up.
  *
  * Make sure any DataQuery instances you are building are BOTH created & executed between start()
  * and done() because augmentDataQueryCreate and augmentSQL happens there.
@@ -18,8 +15,6 @@ use Translatable;
 class ContentReviewCompatability
 {
     const SUBSITES = 0;
-
-    const TRANSLATABLE = 1;
 
     /**
      * Returns the state of other modules before compatibility mode is started.
@@ -29,18 +24,12 @@ class ContentReviewCompatability
     public static function start()
     {
         $compatibility = [
-            self::SUBSITES     => null,
-            self::TRANSLATABLE => null,
+            self::SUBSITES => null,
         ];
 
         if (ClassInfo::exists(Subsite::class)) {
             $compatibility[self::SUBSITES] = Subsite::$disable_subsite_filter;
             Subsite::disable_subsite_filter(true);
-        }
-
-        if (ClassInfo::exists(Translatable::class)) {
-            $compatibility[self::TRANSLATABLE] = Translatable::locale_filter_enabled();
-            Translatable::disable_locale_filter();
         }
 
         return $compatibility;
@@ -53,10 +42,6 @@ class ContentReviewCompatability
     {
         if (class_exists(Subsite::class)) {
             Subsite::$disable_subsite_filter = $compatibility[self::SUBSITES];
-        }
-
-        if (class_exists(Translatable::class)) {
-            Translatable::enable_locale_filter($compatibility[self::TRANSLATABLE]);
         }
     }
 }
