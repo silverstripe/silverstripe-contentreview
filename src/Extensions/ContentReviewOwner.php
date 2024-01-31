8722a49323761f0b7cebe40cb91ad0b071be0f86
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-01-31 15:17:04 +1300

    MNT Run module-standardiser

--- src/Extensions/ContentReviewOwner.php
+++ src/Extensions/ContentReviewOwner.php
@@ -5,11 +5,12 @@ namespace SilverStripe\ContentReview\Extensions;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\ORM\ManyManyList;
 use SilverStripe\Security\Group;
 use SilverStripe\Security\Member;
 
 /**
- * @method SilverStripe\ORM\ManyManyList<SiteTree> SiteTreeContentReview()
+ * @method ManyManyList<SiteTree> SiteTreeContentReview()
  *
  * @extends DataExtension<Group|Member>
  */
